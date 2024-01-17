7d9416c69cc9606ad7de9a34ba429259f4674c4e
Ivan <lavet13@mail.ru>
2024-01-18 01:18:54 +0300

    naming the scripts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
 import 'dotenv/config';
-
-console.log('text');
-
-const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-const data = response.json();
-console.log({ data });
-
-type Something = {
-  name: string;
-};
-
-let something: Something = {
-  name: '123',
-};
-
-console.log({ something });
